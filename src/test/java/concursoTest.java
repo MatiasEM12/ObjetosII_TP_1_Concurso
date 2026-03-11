f663f8d7d1cbeb06ea4b6ed12f5709cc172eb377
MatiasEM12 <matiasemimellado@gmail.com>
2026-03-11 20:00:52 -0300

    test

--- src/test/java/concursoTest.java
+++ src/test/java/concursoTest.java
@@ -1,5 +1,10 @@
 
+import Entities.Concurso;
+import Entities.Inscripcion;
+import Entities.Participante;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -7,7 +12,15 @@ public class concursoTest {
 
     @Test
     public void inscribirParticipante(){
+        //setup
+        var Concurso = new Concurso("Concurso de Programación", LocalDate.of(2025, 1, 1),
+                LocalDate.of(2024, 1, 31)); // la fecha no tiene que depender del dia actual
 
+        var Participante = new Participante("Juan Perez", "12345678");
+        var Inscripcion = new Inscripcion(Participante, LocalDate.now());
+
+        var e = assertThrows(IllegalArgumentException.class, () -> Concurso.nuevaInscripcion(Inscripcion));
+        assertEquals("El participante se encuentra inscripto", e.getMessage());
 
     }
 
