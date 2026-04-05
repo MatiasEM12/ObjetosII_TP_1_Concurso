54b36b64daef50d8a0bbe9836d218405a0b712f3
MatiasEM12 <matiasemimellado@gmail.com>
2026-04-05 12:03:34 -0300

    Refactor lambda DAOJDBC

--- src/main/java/backend/InscripcionDAOJDBC.java
+++ src/main/java/backend/InscripcionDAOJDBC.java
@@ -8,31 +8,61 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
-public class InscripcionDAOJDBC extends InscripcionDAO {
+
+
+
+public class InscripcionDAOJDBC implements InscripcionDAO {
+
+
+    public static final String INSERTAR = "insertar";
+    public static final String ACTUALIZAR = "actualizar";
+    public static final String FINDALL = "listar";
+    public static final String TRUNCATE = "truncate";
+    private <T> T conexion(Function<Connection, T> criterio, String accion){
+        T resultado;
+        try (Connection connection =ConnectionManager.getConnection()) {
+            connection.setAutoCommit(false);
+
+
+            resultado=criterio.apply(connection);
+
+
+            try {
+                connection.commit();
+            } catch (SQLException e) {
+                connection.rollback();
+                throw new RuntimeException("Error al " + accion + " usuario", e);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error al " + accion + " usuario", e);
+        }
+        return resultado;
+    }
+
     @Override
     public void create(Inscripcion inscripcion) {
         if(inscripcion==null)throw  new IllegalArgumentException("La inscripcion es nulla,no se puede persistir");
 
+
         final String SQL="INSERT INTO inscripciones (id,hora_inscripcion,id_concurso,id_participante) VALUES (?,?,?,?)";
 
-        try(Connection conn=ConnectionManager.getConnection();
-            PreparedStatement st= conn.prepareStatement(SQL)){
-
+        this.conexion((connection) -> {;
             Date sqlDate = Date.valueOf(inscripcion.getFechaInscripcion());
+            try(PreparedStatement statement = connection.prepareStatement(SQL)) {
 
-            st.setString(1,inscripcion.getId());
-            st.setDate(2,sqlDate);
-            st.setString(3,inscripcion.getConcursoId());
-            st.setString(4,inscripcion.getParticipanteId());
-            st.executeUpdate();
+                statement.setString(1, inscripcion.getId());
+                statement.setDate(2, sqlDate);
+                statement.setString(3, inscripcion.getConcursoId());
+                statement.setString(4, inscripcion.getParticipanteId());
+                statement.executeUpdate();
+                return null;
+            } catch (SQLException e) {
+                throw new RuntimeException("Error al preparar la consulta de " + INSERTAR + " usuario", e);
+            }
+        }, INSERTAR);
 
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally{
-            Backend.ConnectionManager.disconnect();
-        }
     }
 
     @Override
@@ -63,32 +93,44 @@ public class InscripcionDAOJDBC extends InscripcionDAO {
     @Override
     public List<String> findAllInscriptos() {
 
-        List<String>inscriptos= new ArrayList<>();
-        final String SQL="SELECT * FROM inscripciones ";
-
-        try(Connection conn=ConnectionManager.getConnection();
-            Statement statement = conn.createStatement();
-            ResultSet rs = statement.executeQuery(SQL)){
-
-            while(rs.next()) {
-
-                LocalDate localDate = rs.getDate("hora_inscripcion").toLocalDate();
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-                String horaString = localDate.format(formatter);
-                String inscripto= horaString+", "+ rs.getString("id_participante") +", "+rs.getString("id_concurso");
-
-                inscriptos.add(inscripto);
+        final String SQL = "SELECT * FROM inscripciones ";
 
 
+        return this.conexion((connection) -> {
+            List<String> inscriptos = new ArrayList<>();
+            try (PreparedStatement statement = connection.prepareStatement(SQL);
+                 ResultSet rs = statement.executeQuery(SQL)) {
+
+                while (rs.next()) {
+
+                    LocalDate localDate = rs.getDate("hora_inscripcion").toLocalDate();
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+                    String horaString = localDate.format(formatter);
+                    String inscripto = horaString + ", " + rs.getString("id_participante") + ", " + rs.getString("id_concurso");
+
+                    inscriptos.add(inscripto);
+
+
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
             }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally{
-            ConnectionManager.disconnect();
-        }
+            return inscriptos;
+        }, FINDALL);
+    }
+    @Override
+    public void truncateTabla(){
+        final String SQL = "TRUNCATE TABLE inscripciones";
 
-        return inscriptos;
+        this.conexion((connection) -> {
+            try (PreparedStatement statement = connection.prepareStatement(SQL)) {
+                statement.executeUpdate();
+                return null;
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }, TRUNCATE);
     }
 
 
